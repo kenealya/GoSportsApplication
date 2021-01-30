847dbe22b93c692bd80726b662352648b6499c63
Andrew <Andrew@DESKTOP>
2021-01-30 17:11:52 +1100

    Added values into HTML and commented the assumptions

--- ClientApp/src/app/cricket/cricket.component.ts
+++ ClientApp/src/app/cricket/cricket.component.ts
@@ -1,11 +1,29 @@
-﻿import { Component } from '@angular/core';
+﻿import { Component, OnInit } from '@angular/core';
 import { ICricketDetails } from './CricketDetails';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { DatePipe } from '@angular/common'
 
 @Component({
     selector: 'app-cricket',
     templateUrl: './cricket.component.html',
 })
 
-export class CricketComponent {
+export class CricketComponent implements OnInit {
+    currentDate: string;
+    constructor(private http: HttpClient, public datepipe: DatePipe) { }
     cricketDetails: ICricketDetails;
+
+    cricketURL: string; 
+
+    //Assign the current Date as a string object, the user shouldn't be able to alter this
+    ngOnInit() {
+        this.currentDate = new Date().toLocaleDateString();
+    }
+
+    //Mock up of method to POST the user data to the backend
+    saveDetails(cricketDetails: ICricketDetails): Observable<ICricketDetails> {
+        return this.http.post<ICricketDetails>(this.cricketURL, cricketDetails)
+            .pipe();
+    }
 }
