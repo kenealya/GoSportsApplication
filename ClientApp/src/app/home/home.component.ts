ce69a7b9a0e65906e7f27f7904e44436478478a3
Andrew <Andrew@DESKTOP>
2021-01-30 16:38:30 +1100

    Basic Mock of the homepage

--- ClientApp/src/app/home/home.component.ts
+++ ClientApp/src/app/home/home.component.ts
@@ -1,8 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+    data = [];
+
+    constructor(private apiService: ApiService, private route: Router) { }
+
+    //Currently just a mock  of a subscription service
+    ngOnInit() {
+        //this.apiService.get().subscribe((data: any[]) => {
+        //    this.data = data;
+        //})
+
+        this.data = [{ gameName: 'AUS Vs ENG'}]
+    }
+
+    //basic navigation to switch to cricket.component screen
+    cricketGame() {
+        this.route.navigate(['../cricket/cricket.component']);
+    }
 }
