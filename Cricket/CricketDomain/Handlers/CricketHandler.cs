b3c4e57f8d19db9ba7848c4426c7ae311f11f24d
Andrew Kenealy <andrew_kenealy@dialog.com.au>
2021-02-02 12:23:37 +1100

    added mock of a back-end saving and database factory helper

--- Cricket/CricketDomain/Handlers/CricketHandler.cs
+++ Cricket/CricketDomain/Handlers/CricketHandler.cs
@@ -1,10 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Text;
 
 namespace CricketDomain.Handlers
 {
-    class CricketHandler
+    public class CricketHandler
     {
+
+        [Route("Handlers/Save")]
+        public void SaveCricketDetails(CricketDomain cricketDomain)
+        {
+            //Mock of a save to a NHibernate domain tied to the DB
+            /*ISession session = NHibernateHelper.GetCurrentSession();
+            try
+            {
+                using (ITransaction t = session.BeginTransaction())
+                {
+                    session.Save(princess);
+                    t.Commit();
+                }
+            }
+            finally
+            {
+                NHibernateHelper.CloseSession();
+            }
+             */
+        }
     }
 }
